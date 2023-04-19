c1040566968e358889ffc46aa32e2013a121e614
carlos <junior.pessoaagradavel@gmail.com>
2023-04-18 22:00:05 -0300

    nada a ver

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let array 
+/* et array 
 console.log('a. ',array)
 
 array = null
@@ -15,3 +15,35 @@ console.log('e.', array)
 
 const valor = array[i+6]
 console.log('f.',valor )
+
+const frase = ("Vai ter Prova!")
+console.log(frase.toUpperCase().replaceAll("A", "I",),frase.length)
+
+//Exercicio1
+//1. faça um programa que pergunte seu nome e seu email.
+//em seguida, imprima no console as segunte mensagem 
+
+//o email "amail do usuario" foi cadastrado com sucesso
+// seja bem vindo(a), "nome usuario"
+
+let nomeUsuario= prompt("digite seu nome ")
+let email= prompt("digite seu email")
+
+console.log("o E-mail " + email.trim()+ "foi cadastrado com sucesso")
+console.log("seja bem vindo(a) " + nomeUsuario ) */
+
+let listaDeTarefas 
+
+
+let  listaDeTarefas1= prompt("digite a terefa um  ") 
+let  listaDeTarefas2= prompt("digite a terefa dois ") 
+let listaDeTarefas3= prompt("digite a terefa tres ") 
+
+ listaDeTarefas=[listaDeTarefas1,listaDeTarefas2,listaDeTarefas3]
+
+ console.log(listaDeTarefas)
+ i= Number(prompt("digite um numero de 0 a 2"))
+ listaDeTarefas.splice[i+2]
+ console.log(listaDeTarefas)
+
+
