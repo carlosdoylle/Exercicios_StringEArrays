5f04676668d8e2462adfadbe5827aeba232400c8
carlos <junior.pessoaagradavel@gmail.com>
2023-04-19 21:23:20 -0300

    atualizaçao de atividades

--- script.js
+++ script.js
@@ -32,18 +32,51 @@ let email= prompt("digite seu email")
 console.log("o E-mail " + email.trim()+ "foi cadastrado com sucesso")
 console.log("seja bem vindo(a) " + nomeUsuario ) */
 
-let listaDeTarefas 
+//ATIVIDADE 0
 
-
-let  listaDeTarefas1= prompt("digite a terefa um  ") 
+/* let  listaDeTarefas1= prompt("digite a terefa um  ") 
 let  listaDeTarefas2= prompt("digite a terefa dois ") 
 let listaDeTarefas3= prompt("digite a terefa tres ") 
 
- listaDeTarefas=[listaDeTarefas1,listaDeTarefas2,listaDeTarefas3]
+  let listaDeTarefas=[listaDeTarefas1,listaDeTarefas2,listaDeTarefas3]
 
  console.log(listaDeTarefas)
- i= Number(prompt("digite um numero de 0 a 2"))
- listaDeTarefas.splice[i+2]
- console.log(listaDeTarefas)
+ let indice = prompt("digite um numero de 0 a 2")
+ listaDeTarefas.splice([indice], 1)
+ console.log(listaDeTarefas) */
 
+
+ //ATIVIDADE 1
+//receba um a frase e retorne um array onde cada elemente é 
+//umas das palavras ignorando os espaços vazios
+
+/* let frase = prompt("digite uma frase")
+ frase.trim;
+ let array2 = frase.split(' ')
+alert(array2 + "\n" + array2.length)
+ */
+
+//ATIVIDADE 2 
+/* let arrayFrutas  = [ "banana", "maorango","abacaxi","laranja","ameixa"]
+let indice = arrayFrutas.indexOf("abacaxi")
+console.log(indice, arrayFrutas.length) */
+
+//ATIVIDADE 3
+
+/*  let comidas = ['pizza', 'lasanha' , 'caviar' , 'xis' , 'massa']
+alert (comidas[0] + '\n'+ comidas[1] + '\n' + comidas[2] + '\n' + comidas[3] + '\n' + comidas[4] + '\n') 
+
+ comidas[1] = prompt("informe sua comida preferida")
+console.log(comidas) */
+
+//ATIVIDADE 4
+
+/* let num1 = prompt("digite numero 1:")
+let num2 = prompt("digite numero 2:")
+let num3 = prompt("digite numero 3:")
+
+let array =[num1,num2,num3]
+console.log(array)
+
+console.log(array.reverse()) */
 
